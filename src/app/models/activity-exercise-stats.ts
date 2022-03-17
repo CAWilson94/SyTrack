762cd736a7a4c6531ac8b8e48c988317ef7efaf4
CAWilson94 <charlotteawilson.94@gmail.com>
2022-03-17 09:45:16 +0000

    exercise stats split from general activity stats

---
+++ src/app/models/activity-exercise-stats.ts
@@ -0,0 +1,5 @@
+export interface ActivityExerciseStats { 
+    steps: number, 
+    indoorBike: number, 
+    outdoorBike: number, 
+}
--- src/app/models/activity-stats.ts
+++ src/app/models/activity-stats.ts
@@ -1,8 +1,5 @@
 export interface ActivityStats{ 
     driving: number, 
-    steps: number, 
-    indoorBike: number, 
-    outdoorBike: number, 
     carPassanger: number, 
     workingSeated: number, 
 }
